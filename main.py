51c3f09cd759d4029a6258382879c59f8a987040
Lucy Taylor <77295077+lucyt245@users.noreply.github.com>
2021-08-18 17:29:13 +0100

    Update main.py
    removed hangman and added to separate file

--- main.py
+++ main.py
@@ -10,10 +10,9 @@ def menu_system():
         print("------------------------------")
         print("1 - Mad libs")
         print("2 - Month dictionary")
-        print("3 - Hangman")
-        print("4 - Add word")
-        print("5 - Prime numbers")
-        print("6 - Number add")
+        print("3 - Add word")
+        print("4 - Prime numbers")
+        print("5 - Number add")
         print("0 - Exit")
 
         choice = int_validation()
@@ -24,12 +23,10 @@ def menu_system():
         elif choice == 2:
             month_dictionary()
         elif choice == 3:
-            hangman()
-        elif choice == 4:
             add_word()
-        elif choice == 5:
+        elif choice == 4:
             prime_nums()
-        elif choice == 6:
+        elif choice == 5:
             num_add()
         elif choice == 0:
             exit = True
@@ -150,123 +147,6 @@ def word_get():
     hint = hint_list[number]
     # print(hint)
     return word_choice, hint
-
-
-def guess_validate(wrong_guesses, word_choice, word_display, guessed):
-    guessed_before = False
-    user_guess = input("Please guess a letter or the word: ")
-    print(guessed)
-
-    # checks that the letter/word hasn't already been guessed
-    for i in range(0, len(guessed)):
-        if guessed[i] == user_guess:
-            guessed_before = True
-
-    # isaplha() checks that the input is a string made of letter
-    if user_guess.isalpha() and not guessed_before:
-        guessed.append(user_guess)
-        # print(guessed)
-        # returns it as upper case
-        return user_guess.upper()
-    elif guessed_before:
-        print("You've already guessed that")
-        hangman_guessing(wrong_guesses, word_choice, word_display, guessed)
-    else:
-        print("Invalid guess")
-        hangman_guessing(wrong_guesses, word_choice, word_display, guessed)
-
-
-def hangman_display(wrong_guesses):
-    dead = ['''
-              o
-             /|\\
-              |
-             / \\
-            ''',
-            '''
-              o
-             /|\\
-              |
-             / 
-            ''',
-            '''
-              o
-             /|\\
-              |
-
-            ''',
-            '''
-              o
-             /|
-              |
-
-            ''',
-            '''
-              o
-              |
-              |
-
-            ''',
-            '''
-              o
-              |
-              
-             
-            ''']
-
-    return dead[wrong_guesses]
-
-
-def hangman():
-    print("---------")
-    print(" Hangman")
-    print("---------")
-
-    word_choice, hint = word_get()
-    word_display = []
-    for i in range(0, len(word_choice)):
-        word_display.append("_")
-        i += 1
-
-    wrong_guesses = 0
-    guessed = []
-    print(hangman_display(wrong_guesses))
-    print(word_display)
-
-    hangman_guessing(wrong_guesses, word_choice, word_display, guessed)
-
-
-def hangman_guessing(wrong_guesses, word_choice, word_display, guessed):
-    while wrong_guesses < 5:
-        guessed_letter = False
-        user_guess = guess_validate(wrong_guesses, word_choice, word_display, guessed)
-        if len(user_guess) > 1:
-            if user_guess == word_choice:
-                print("Congratulations!")
-                print("You guessed the word!")
-                menu_system()
-            else:
-                print("Incorrect guess!")
-                wrong_guesses += 1
-                print(hangman_display(wrong_guesses))
-                print(word_display)
-
-        else:
-            for i in range(0, len(word_choice)):
-                if user_guess == word_choice[i]:
-                    print("You guessed a letter!")
-                    guessed_letter = True
-                    word_display[i] = user_guess
-
-            if not guessed_letter:
-                print("Incorrect guess!")
-                wrong_guesses += 1
-
-            print(hangman_display(wrong_guesses))
-            print(word_display)
-
-    print("You had too many wrong guesses and DIED")
-    print("Better luck next time!")
 
 
 def int_validate():
